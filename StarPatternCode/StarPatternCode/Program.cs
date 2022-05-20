956fb7fbf20496ec0a95a6a31ad61f07fcb94bfd
kiran04121997 <bhosalekiran259@gmail.com>
2022-05-20 11:40:04 +0530

    [kiran_bhosale]star_pattern

---
+++ StarPatternCode/StarPatternCode/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarPatternCode
+{
+    class StarPattern
+    {
+        public static void Main()
+        {
+
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int i = 1; i <= 5; i++)   // to combine both make i<=4 and In inner for loop make j<=4
+            {
+                for (int j = i; j <= 5; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
